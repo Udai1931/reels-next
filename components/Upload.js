e6973fee4fdc6ccc1d37a931417fbd08c0e5a43d
Udai Gupta <udaigupta19311@gmail.com>
2022-02-12 17:47:15 +0530

    start icon

--- components/Upload.js
+++ components/Upload.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
+import BackupIcon from '@mui/icons-material/Backup';
 
 function Upload() {
     return (
-        <div>
-            <Button variant="outlined" color='secondary' component="span">
-                <Input type="file" accept="image/*" style={{ display: 'none' }} />
-                Upload
-            </Button>
-            <Button variant="outlined" margin='dense' fullWidth component="span" sx={{ marginTop: "1rem" }}>
-                <Input accept="image/*" style={{ display: 'none' }} />
+        <div style={{ width: "100vw", height: "10vh", display: "flex", justifyContent: "center", alignItems: "center", fontWeight: "bold" }}>
+            <Button variant="outlined" color='secondary' component="label" style={{ fontWeight: "bold",width:"15rem"}} startIcon={<BackupIcon />}>
+                <input type="file" accept="video/*" style={{ display: 'none' }} />
                 Upload
             </Button>
         </div>
